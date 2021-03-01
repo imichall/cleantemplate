5b460342f2659b5751379aee6a7fee4498d33422
Michal Svoboda <iimichal.svoboda@gmail.com>
2021-03-01 23:24:00 +0100

    fix validation + style

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,10 @@
 import jQuery from "jquery";
 import slick from "slick-carousel";
+import Validate from "jquery-validation";
 
 window.$ = window.jQuery = jQuery;
 
-$('.testimonials').slick({
+$(".testimonials").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
@@ -12,37 +13,52 @@ $('.testimonials').slick({
     swipe: true,
     responsive: [
         {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-          }
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+            },
         },
         {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+            },
         },
         {
-          breakpoint: 325,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            arrows: false,
-            dots: true,
-          }
-        }
-      ]
-  });
+            breakpoint: 325,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                dots: true,
+            },
+        },
+    ],
+});
 
-  $(function() {
-    $("#Navbar-Menu button").on('click', function() {
-        $('.Navigation_Link').toggleClass('isOpen');
+$(function () {
+    $("#Navbar-Menu button").on("click", function () {
+        $(".Navigation_Link").toggleClass("isOpen");
     });
-    /* $('#Navbar-Menu button').on('click', function(){
-        $(this).toggleClass('isOpen');
-    }); */
+
+    $(function () {
+        $("form[name='newsletter']").validate({
+            rules: {
+                email: "required",
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                email: "Please enter a valid email address",
+            },
+            submitHandler: function (form) {
+                form.submit();
+            },
+        });
+    });
 });
